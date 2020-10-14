cb3c97f801f52f943c68ddcab2651d59f2fee65f
Ann <62781273+Dokoru@users.noreply.github.com>
2020-10-14 06:19:02 +0300

    Delete ColorService.java

--- src/chess/service/ColorService.java
+++
@@ -1,9 +0,0 @@
-package chess.service;
-
-import chess.model.pieces.Color;
-
-public class ColorService {
-    public Color reversColor(Color color) {
-        return color == Color.WHITE ? Color.BLACK : Color.WHITE;
-    }
-}
